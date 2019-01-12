9773771b5d250d950c2a62c3d61e6c1e9fb4b811
lacsinagramar <lacsinagramar@gmail.com>
2019-01-13 07:49:52 +0800

    Fix side nav and created stall maintenance

--- app/modules/admin/routes.js
+++ app/modules/admin/routes.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
-
+//GET
 router.get('/', (req, res) => {
-    res.render('admin/views/index');
+    res.render('admin/views/index', {url: req.url});
 });
+router.get('/login', (req, res) => {
+    res.render('admin/views/login');
+});
+router.get('/stall', (req, res) => {
+    res.render('admin/views/stall', {url: req.url})
+})
+
+//POST
+router.post('/login', (req, res) => {
+    if(req.body.user == 'admin' && req.body.pass == 'admin'){
+        req.session.admin = true;
+        console.log(req.session);
+        return res.send({valid: true})
+    }
+    else{
+        return res.send({valid: false})
+    }
+});
+router.post('/addstall', (req, res) => {
+    console.log(req.body)
+    res.redirect('/admin/stall')
+})
 
 exports.admin = router;

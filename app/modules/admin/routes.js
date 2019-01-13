d13bfef515fe3b72dec046bf5cba5e9e19348cc2
lacsinagramar <lacsinagramar@gmail.com>
2019-01-13 10:57:36 +0800

    added jquery mask, added edit stall

--- app/modules/admin/routes.js
+++ app/modules/admin/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../../lib/database')();
 
 //GET
 router.get('/', (req, res) => {
@@ -9,7 +10,11 @@ router.get('/login', (req, res) => {
     res.render('admin/views/login');
 });
 router.get('/stall', (req, res) => {
-    res.render('admin/views/stall', {url: req.url})
+    db.query('SELECT * FROM tbl_stall', (err, results) =>{
+        if(err) console.log(err)
+
+        return res.render('admin/views/stall', {url: req.url, stalls:results})
+    })
 })
 
 //POST
@@ -25,7 +30,32 @@ router.post('/login', (req, res) => {
 });
 router.post('/addstall', (req, res) => {
     console.log(req.body)
-    res.redirect('/admin/stall')
+    db.query('INSERT INTO tbl_stall VALUES(?, ?, 0)', [req.body.stallId, req.body.stallType], (err, results) => {
+        if(err) console.log(err)
+
+        return res.redirect('/admin/stall')
+    })
+});
+router.post('/stall-id-check', (req, res) => {
+    let originalStall = req.body.originalStallId;
+    if(typeof req.body.originalStallId == 'undefined'){
+        originalStall = '';
+    } 
+    console.log('sent: ', originalStall)
+    db.query('SELECT strId FROM tbl_stall WHERE strId = ? AND strId!= ?', [req.body.stallId, originalStall], (err, results) => {
+        if(err) console.log(err)
+
+        if(results.length>0) return res.send('false')
+        else return res.send('true')
+    })
 })
+router.post('/edit-stall', (req, res) => {
+    console.log(req.query)
+    db.query('UPDATE tbl_stall SET strId = ? , booStallType = ? WHERE strId = ? ', [req.body.stallId, req.body.stallType, req.query.stallId], (err, results) => {
+        if(err) console.log(err)
+
+        return res.redirect('/admin/stall');
+    })
+});
 
 exports.admin = router;

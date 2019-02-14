63a3ec5ecd07c90c9ba7923be0bc8ecd3de99539
lacsinagramar <lacsinagramar@gmail.com>
2019-02-14 19:08:15 +0800

    bills for lessee portal done

--- app/modules/auth/middlewares/auth.js
+++ app/modules/auth/middlewares/auth.js
@@ -1,5 +1,4 @@
 exports.hasAdmin = (req, res, next) => {
-    console.log(req.url)
     if (req.session && req.session.admin) return next();
     else if (req.session && req.session.staff) return res.redirect('/admin?unauthorized')
     return res.redirect(`/admin/login?redirect=${req.url}`);
@@ -11,4 +10,8 @@ exports.hasNoAdminOrStaff = (req, res, next) => {
 exports.hasAdminOrStaff = (req, res, next) => {
     if (req.session && (req.session.admin || req.session.staff)) return next();
     return res.redirect(`/admin/login?redirect=${req.url}`);
+}
+exports.hasLessee = (req, res, next) => {
+    if (req.session && req.session.lessee) return next();
+    return res.redirect(`/login`);
 }
--- app/modules/home/routes.js
+++ app/modules/home/routes.js
@@ -1,9 +1,98 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../../lib/database')();
+const middleware = require('../auth/middlewares/auth');
+const moment = require('moment');
 
+//GET
+router.get('/', middleware.hasLessee, (req, res) => {
+    function getBills(type, contractId){
+        let query = ``
+        if(type == 'E'){
+            query = `SELECT * FROM tbl_electric_lessee_bill WHERE intContractId = ?`
+        }
+        else if(type == 'W'){
+            query = `SELECT * FROM tbl_water_lessee_bill WHERE intContractId = ?`
+        }
+        else if(type == 'R'){
+            query = `SELECT * FROM tbl_rental_bill WHERE intContractId = ?`
+        }
+        return new Promise(function (resolve, reject) {
+            db.query(query, contractId, (err, results) => {
+                if(err) console.log(err)
 
-router.get('/', (req, res) => {
-    res.render('home/views/index');
+                resolve(results)
+            })
+        })
+    }
+    db.query('SELECT * FROM tbl_contract WHERE strLesseeId = ? AND booContractStatus = 0', [req.session.lessee.strId], (err, results) => {
+        if(err) console.log(err)
+        let finalBills = []
+        if(results.length > 0){
+            for(let h = 0; h < results.length; h++){
+                getBills('E', results[h].intId).then(electricBills =>{
+                    for(let j = 0; j<electricBills.length; j++){
+                        electricBills[j].type = 'E'
+                        electricBills[j].datDueDate = moment(electricBills[j].datDueDate).format('YYYY-MM-DD')
+                        finalBills.push(electricBills[j])
+                    }
+                    getBills('W', results[h].intId).then(waterBills =>{  
+                        for(let k = 0; k<waterBills.length; k++){
+                            waterBills[k].type = 'W'
+                            waterBills[k].datDueDate = moment(waterBills[k].datDueDate).format('YYYY-MM-DD')
+                            finalBills.push(waterBills[k])
+                        }
+                        getBills('R', results[h].intId).then(rentalBills =>{
+                            for(let l = 0; l<rentalBills.length; l++){
+                                rentalBills[l].type = 'R'
+                                rentalBills[l].datDueDate = moment(rentalBills[l].datDueDate).format('YYYY-MM-DD')
+                                finalBills.push(rentalBills[l])
+                                if(l == rentalBills.length - 1){
+                                    finalBills.sort(function (current, next){
+                                        if(moment(current.datDueDate).isAfter(next.datDueDate)){
+                                            return -1;
+                                        }
+                                        if(moment(current.datDueDate).isBefore(next.datDueDate)){
+                                            return 1;
+                                        }
+                                        if(moment(current.datDueDate).isSame(next.datDueDate)){
+                                            return 0;
+                                        }
+                                    })
+                                    return res.render('home/views/index', {bills: finalBills, session: req.session.lessee})
+                                }
+                            }
+                        })
+                    })
+                })
+            }
+        }
+    })
+})
+router.get('/login', (req, res) => {
+    return res.render('home/views/login');
 });
+router.get('/logout', (req, res) => {
+    delete req.session.lessee
+    res.redirect('/login')
+    console.log(req.session)
+})
+// END GET
+
+//POST
+router.post('/login', (req, res) => {
+    db.query('SELECT * FROM tbl_lessee WHERE strUsername = ? AND strPassword = ? AND booIsDeleted = 0',[req.body.user, req.body.pass], (err, results) => {
+        if(err) console.log(err)
+
+        if(results.length > 0){
+            req.session.lessee = results[0]
+            return res.send({valid: true})
+        }
+        else{
+            return res.send({valid: false})
+        }
+    })
+})
+//END POST
 
 exports.index = router;
